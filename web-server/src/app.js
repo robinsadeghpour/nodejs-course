a93395a97adcf71a4b07ea5332399cbad20677d9
robinsadeghpour <r.sadegh.f@gmail.com>
2022-05-20 20:35:50 +0200

    hbs help

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,18 +2,39 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
 const options = {
     extensions: ['html']
 }
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Static directory to serve
 app.use(express.static(publicDirectoryPath, options))
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
         name: 'Andrew Mead'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text'
     })
 })
 
